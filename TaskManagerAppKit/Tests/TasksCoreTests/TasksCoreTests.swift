f2dc25b865df94bd1c958ee3deb9923f5bec16f5
Sarah Lichter <lichter.sarahe@gmail.com>
2022-01-18 13:13:56 -0600

    added TasksCore to state

---
+++ TaskManagerAppKit/Tests/TasksCoreTests/TasksCoreTests.swift
@@ -0,0 +1,35 @@
+//
+//  TasksCoreTests.swift
+//  
+//
+//  Created by Sarah Lichter on 1/18/22.
+//
+
+import XCTest
+import ComposableArchitecture
+import Models
+import AppCore
+@testable import TasksCore
+
+class TasksCoreTests: XCTestCase {
+
+    func testCreateTaskAddsTaskToStateState() {
+        let state = AppState()
+        let store = TestStore(
+            initialState: state,
+            reducer: appReducer,
+            environment: AppEnvironment()
+        )
+        let newTaskTitle = "Do Laundry"
+        let taskId = UUID()
+        let projectId = UUID()
+
+        let expectedTask = Task(id: taskId, title: newTaskTitle, projectId: projectId)
+
+        store.send(.tasks(.createTask(taskId, newTaskTitle, projectId))) {
+            $0.tasks = [expectedTask]
+
+        }
+    }
+
+}
